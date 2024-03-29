ff538004580a4c53f04ea444a9a8e25b17c91681
oladokunlt <lukmanoladokun86@gmail.com>
2024-03-29 13:32:33 +0100

    Chore: Added labels card for overview section.

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,7 +1,56 @@
 import React from "react";
-
+import CardOverview from "../utilities/CardOverview";
+import tradeUp from "../images/trade-up.png";
+import tradeDown from "../images/trade-down.png";
 const Home = () => {
-  return <div>Overview</div>;
+  return (
+    <div className="flex gap-[24px]">
+      <div
+        className="bg-[url('/revenue.png')] inline-flex"
+        style={{ backgroundRepeat: "no-repeat", backgroundSize: "100% 100%" }}
+      >
+        <CardOverview
+          amount="20,000,000"
+          revenue="revenue"
+          icon={tradeUp}
+          month="March  10%"
+        />
+      </div>
+      <div
+        className="bg-[url('/sales.png')] inline-flex"
+        style={{ backgroundRepeat: "no-repeat", backgroundSize: "100% 100%" }}
+      >
+        <CardOverview
+          amount="42"
+          revenue="Total sales"
+          icon={tradeUp}
+          month="March  10%"
+        />
+      </div>
+      <div
+        className="bg-[url('/employees.png')] inline-flex"
+        style={{ backgroundRepeat: "no-repeat", backgroundSize: "100% 100%" }}
+      >
+        <CardOverview
+          amount="21"
+          revenue="Employees"
+          icon={tradeDown}
+          month="March  20%"
+        />
+      </div>
+      <div
+        className="bg-[url('/clients.png')] inline-flex"
+        style={{ backgroundRepeat: "no-repeat", backgroundSize: "100% 100%" }}
+      >
+        <CardOverview
+          amount="24"
+          revenue="24"
+          icon={tradeUp}
+          month="March  10%"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/components/utilities/CardOverview.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const CardOverview = ({ revenue, amount, month, icon }) => {
+  return (
+    <>
+      <div className="p-8 inline-flex flex-col gap-[24px] rounded">
+        <p className="font-medium text-[#E6E6E6]">{revenue}</p>
+        <p className="font-bold text-[24px] text-white">{amount}</p>
+        <p className=" text-[#E6E6E6]  flex items-center">
+          {month}
+          <span>
+            <img src={icon} alt="icon" />
+          </span>
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default CardOverview;
